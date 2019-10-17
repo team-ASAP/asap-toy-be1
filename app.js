1b5df92727b657ef7c9d4daaa2edf78be89517db
HYEON <helloworld.hellohyeon@gmail.com>
2019-10-17 20:30:51 +0900

    project schema, get post add

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
