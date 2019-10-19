55f97680e0b7d47f16e04f6da3b9e132a6ac6803
xodms1701 <iostream1701@gmail.com>
2019-10-19 14:12:44 +0900

    기본 설정

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: String,
+    id: String,
     passwd: String,
     data: Object
 });
