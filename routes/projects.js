6eebbb111ed01f1685fb89846af8dac608aec298
HYEON <helloworld.hellohyeon@gmail.com>
2019-10-27 11:43:02 +0900

    delete structure

--- routes/projects.js
+++ routes/projects.js
@@ -8,6 +8,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/list', (req, res, next) => {
+  Project.find({"isDeleted" : false}, (err, projects) => {
+    res.json(util.successTrue(projects));
+  })
+
+});
+
 // 우선 컬럼 세 개만 넣어서 insert 테스트 하였습니다 :) 
 // 이미 projects라는게 경로에 명시되어 있기 때문에 create로 변경하였습니다.
 router.post('/create', (req, res, next) => {
@@ -35,6 +42,21 @@ router.post('/create', (req, res, next) => {
       return;
     }
     res.json(util.successTrue(value));
+  });
+});
+
+// project id 받아서 isDeleted true로 변경
+router.post('/delete', (req, res, next) => {
+
+  Project.update({"_id":req.body._id}, { isDeleted : true }, (err, value) => {
+    console.log(req.body.projects_id);
+    
+    if(err){
+      res.json(util.successFalse(err, "실패"));
+    }
+    else{
+      res.json(util.successTrue(value));
+    }
   });
 });
 
