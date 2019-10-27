83ac0389f2fd0ffe05af50b76ba0c8c3853f9aab
HYEON <helloworld.hellohyeon@gmail.com>
2019-10-27 15:07:44 +0900

    date modify

--- routes/projects.js
+++ routes/projects.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var dateUtils = require('date-utils');
 const Project = require('../model/project');
 const util = require('../util');
 
@@ -19,14 +20,19 @@ router.get('/list', (req, res, next) => {
 // 이미 projects라는게 경로에 명시되어 있기 때문에 create로 변경하였습니다.
 router.post('/create', (req, res, next) => {
   const project = new Project();
+  const newDate = new Date();
+  
 
   //https://docs.mongodb.com/manual/reference/method/Date/
   //required
   project.madeId = req.body.madeId;
   project.title = req.body.title;
-  project.startDate = new Date(req.body.startDate);
-  project.endDate = new Date(req.body.endDate);
-  project.createdDate = new Date(req.body.createdDate);
+
+  // 그냥 new Date(req.body.starDate/endDate)로 넘기면 로컬 시간으로 두ㅡㄹ어가지 않아
+  // format 함수 추가했습니다
+  project.startDate = new Date(req.body.startDate).toFormat('YYYY-MM-DD');
+  project.endDate = new Date(req.body.endDate).toFormat('YYYY-MM-DD');
+  project.createdDate = newDate.toFormat('YYYY-MM-DD');
 
   //unrequired
   if(req.body.maxPeople){
@@ -47,8 +53,9 @@ router.post('/create', (req, res, next) => {
 
 // project id 받아서 isDeleted true로 변경
 router.post('/delete', (req, res, next) => {
+  const newDate = new Date();
 
-  Project.update({"_id":req.body._id}, { isDeleted : true }, (err, value) => {
+  Project.update({"_id":req.body._id}, { isDeleted : true , deletedDate : newDate.toFormat('YYYY-MM-DD')}, (err, value) => {
     console.log(req.body.projects_id);
     
     if(err){
