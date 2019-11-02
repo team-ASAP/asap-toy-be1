7b0cce9da069c3603d4563642936a492ce65ebc2
xodms1701 <iostream1701@gmail.com>
2019-11-02 15:55:44 +0900

    get myinfo

--- routes/auth.js
+++ routes/auth.js
@@ -123,12 +123,19 @@ router.post('/withdrawal', util.isLoggedin, (req, res, next) => {
 //내 정보
 router.get('/me', util.isLoggedin, (req, res, next) => {
     if(req.decoded){
-        const user = {
+        const me = {
             id: req.decoded.id,
-            data: req.decoded.data
+            data: null
         };
 
-        return res.json(util.successTrue(user));
+        User.findOne({ id: me.id }, (err, user)=> {
+            if(err) return res.json(util.successFalse(err, '몽고디비에러'));
+            if(!user) return res.json(tuil.successFalse(err, '일치하는 회원이 없음'));
+
+            me.data = user.data;
+
+            return res.json(util.successTrue(me));
+        });
     }else{
         return res.json(util.successFalse('로그인 상태가 아닙니다.'));
     }
