9505cacbb9a28dec62fc68e096033e6f550a833e
xodms1701 <iostream1701@gmail.com>
2019-11-07 22:57:31 +0900

    master branch's swagger init merge

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const db = require('./db');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const swaggerDoc = require('./swaggerDoc');
 
 const app = express();
 
@@ -12,7 +13,8 @@ db();
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
-const apiDocsRouter = require('./routes/apiDocs');
+
+app.use(swaggerDoc);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
-app.use('/apiDocs', apiDocsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,39 @@ const dormantUser = require('../model/dormantUser');
 const withdrawalUser = require('../model/withdrawalUser');
 const util = require('../util');
 
-router.get('/', (req, res, next) => {
-    res.json(util.successTrue());
+/**
+ * @swagger
+ * /user/:name:
+ *   get:
+ *     summary: 사용자 정보 가져오기
+ *     tags: [User]
+ */
+router.get('/:name', (req, res, next) => {
+  User.find({name : req.params.name }, (err, user) =>{
+    res.json({ user: user });
+  });
 });
 
-router.get('/:id', (req, res, next) => {
-    res.json(util.successTrue(req.params.id));
+/**
+ * @swagger
+ * /user/join/:name/:passwd:
+ *  post:
+ *    summary: 가입
+ *    tags: [User]
+ */
+router.post('/join/:name/:passwd', (req, res, next) => {
+  const user = new User();
+  user.name = req.params.name;
+  user.passwd = req.params.passwd;
+
+  user.save(err =>{
+    if(err){
+      console.error(err);
+      res.json({result: 0});
+      return;
+    }
+    res.json({result:1});
+  });
 });
 
 module.exports = router;
