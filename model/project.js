15e8638ef7332634782636acc01cef4fa7188e1d
HYEON <helloworld.hellohyeon@gmail.com>
2019-11-23 23:30:45 +0900

    modify

--- model/project.js
+++ model/project.js
@@ -3,15 +3,15 @@ const Schema = mongoose.Schema;
 
 // 프로젝트 생성 스키마
 const projectSchema = new Schema({
-    madeId: String,
-    title: String,
-    startDate: Date,
-    endDate: Date,
-    maxPeople: Number,
-    createdDate: Date,
-    deletedDate: Date,
-    modifiedDate: Date,
-    isDeleted: Boolean
+    madeId: String, // 프로젝트 생성자 아이디
+    title: String, // 제목
+    startDate: Date, // 프로젝트 시작 날짜
+    endDate: Date, // 프로젝트 종료 날짜
+    maxPeople: Number, // 최대 인원 
+    createdDate: Date, // 생성된 날짜 - 수정 불가
+    deletedDate: Date, // 삭제된 날짜 - 수정 불가
+    modifiedDate: Date, // 수정된 날짜 
+    isDeleted: Boolean // 삭제된 포스트인지 확인
 });
 
 module.exports = mongoose.model('Project', projectSchema);
--- routes/projects.js
+++ routes/projects.js
@@ -12,8 +12,7 @@ router.get('/', function(req, res, next) {
 router.get('/list', (req, res, next) => {
   Project.find({"isDeleted" : false}, (err, projects) => {
     res.json(util.successTrue(projects));
-  })
-
+  });
 });
 
 // 우선 컬럼 세 개만 넣어서 insert 테스트 하였습니다 :) 
@@ -21,14 +20,13 @@ router.get('/list', (req, res, next) => {
 router.post('/create', (req, res, next) => {
   const project = new Project();
   const newDate = new Date();
-  
 
   //https://docs.mongodb.com/manual/reference/method/Date/
   //required
   project.madeId = req.body.madeId;
   project.title = req.body.title;
 
-  // 그냥 new Date(req.body.starDate/endDate)로 넘기면 로컬 시간으로 두ㅡㄹ어가지 않아
+  // 그냥 new Date(req.body.starDate/endDate)로 넘기면 로컬 시간으로 들어가지 않아
   // format 함수 추가했습니다
   project.startDate = new Date(req.body.startDate).toFormat('YYYY-MM-DD');
   project.endDate = new Date(req.body.endDate).toFormat('YYYY-MM-DD');
@@ -66,5 +64,30 @@ router.post('/delete', (req, res, next) => {
     }
   });
 });
+
+// front에서 원래 입력되어있던 값 수정 폼에 뿌려두고 수정할 부분만 수정한 후 호출시켜서
+// 수정할 수 있는 모든 값 덮어쓰는 방식으로 처리
+
+// master branch에서 마지막 commit 두 개만 가져와서 merge
+router.post('/modify', (req, res, next) => {
+  const newDate = new Date();
+
+  Project.update({"_id":req.body.id, isDeleted: false}, {
+    title: req.body.title,
+    startDate: new Date(req.body.startDate).toFormat('YYYY-MM-DD'),
+    endDate: new Date(req.body.endDate).toFormat('YYYY-MM-DD'),
+    maxPeople: req.body.maxPeople,
+    modifiedDate: newDate.toFormat('YYYY-MM-DD')
+  }, (err, value) => {
+    console.log(req.body.id);
+
+    if(err){
+      res.json(util.successFalse(err, "실패"));
+    }
+    else{
+      res.json(util.successTrue(value));
+    }
+  });
+})
 
 module.exports = router;
