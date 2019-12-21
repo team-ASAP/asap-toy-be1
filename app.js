201e76614c45123c9123c4db18fadab54ad447d2
xodms1701 <iostream1701@gmail.com>
2019-12-21 14:13:31 +0900

    add swagger

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(swaggerDoc);
+
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
