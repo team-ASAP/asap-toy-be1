b99531bc261b638e181d25945c411874113b31c9
HYEON <helloworld.hellohyeon@gmail.com>
2019-12-22 10:18:04 +0900

    conflict solve 2

--- model/project.js
+++ model/project.js
@@ -3,20 +3,13 @@ const Schema = mongoose.Schema;
 
 // 프로젝트 생성 스키마
 const projectSchema = new Schema({
-<<<<<<< HEAD
-    no: Number,
-=======
->>>>>>> 650f53c42f659b0ed4ec840828a3824106ff448c
     madeId: String,
     title: String,
     startDate: Date,
     endDate: Date,
     maxPeople: Number,
     createdDate: Date,
-<<<<<<< HEAD
-=======
     deletedDate: Date,
->>>>>>> 650f53c42f659b0ed4ec840828a3824106ff448c
     modifiedDate: Date,
     isDeleted: Boolean
 });
--- routes/users.js
+++ routes/users.js
@@ -5,40 +5,15 @@ const dormantUser = require('../model/dormantUser');
 const withdrawalUser = require('../model/withdrawalUser');
 const util = require('../util');
 
-<<<<<<< HEAD
-
-/**
- * @swagger
- * /user/:name:
- *   get:
- *     summary: 사용자 정보 가져오기
- *     tags: [User]
- */
-router.get('/:name', (req, res, next) => {
-  User.find({name : req.params.name }, (err, user) =>{
-=======
 // name -> id
 router.get('/:id', (req, res, next) => {
   User.find({id : req.params.id }, (err, user) =>{
->>>>>>> 650f53c42f659b0ed4ec840828a3824106ff448c
     res.json({ user: user });
   });
 });
 
-<<<<<<< HEAD
-
-/**
- * @swagger
- * /user/join/:name/:passwd:
- *  post:
- *    summary: 가입
- *    tags: [User]
- */
-router.post('/join/:name/:passwd', (req, res, next) => {
-=======
 // name -> id
 router.post('/join/:id/:passwd', (req, res, next) => {
->>>>>>> 650f53c42f659b0ed4ec840828a3824106ff448c
   const user = new User();
   user.id = req.params.id;
   user.passwd = req.params.passwd;
@@ -46,17 +21,10 @@ router.post('/join/:id/:passwd', (req, res, next) => {
   user.save(err =>{
     if(err){
       console.error(err);
-<<<<<<< HEAD
       res.send("FAILURE");
       return;
     }
     res.send("SUCCESS");
-=======
-      res.send("SUCCESS");
-      return;
-    }
-    res.send("FAILURE");
->>>>>>> 650f53c42f659b0ed4ec840828a3824106ff448c
   });
 });
 
