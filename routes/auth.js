33f8ccb541f4d8f66d098836aab2bdad15697291
김태은 <kimtaeeun@kimtaeeuns-MacBook-Pro.local>
2019-12-23 23:43:12 +0900

    [router] auth 라우터 내정보보기 위치 이동

--- routes/auth.js
+++ routes/auth.js
@@ -120,25 +120,4 @@ router.post('/withdrawal', util.isLoggedin, (req, res, next) => {
     }
 });
 
-//내 정보
-router.get('/me', util.isLoggedin, (req, res, next) => {
-    if(req.decoded){
-        const me = {
-            id: req.decoded.id,
-            data: null
-        };
-
-        User.findOne({ id: me.id }, (err, user)=> {
-            if(err) return res.json(util.successFalse(err, '몽고디비에러'));
-            if(!user) return res.json(tuil.successFalse(err, '일치하는 회원이 없음'));
-
-            me.data = user.data;
-
-            return res.json(util.successTrue(me));
-        });
-    }else{
-        return res.json(util.successFalse('로그인 상태가 아닙니다.'));
-    }
-});
-
 module.exports = router;
